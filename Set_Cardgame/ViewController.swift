a945b7cf7c1bb76bf8970cb07c2fd77c9be1c148
Rona Bompa <rona.bompa@endava.com>
2022-02-24 17:48:29 +0200

    init

---
+++ Set_Cardgame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Set_Cardgame
+//
+//  Created by Rona Bompa on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
